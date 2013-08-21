81ba5aadb39ebcab0d6358432f71efe4e5c20d5e
Sergey Lukin <contact@sergeylukin.com>
2013-08-20 21:38:09 -0400

    Generate WWW files

---
+++ www/js/index.js
@@ -0,0 +1,27 @@
+(function() {
+  var app;
+
+  app = {
+    initialize: function() {
+      return this.bindEvents();
+    },
+    bindEvents: function() {
+      return document.addEventListener("deviceready", this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+      return app.receivedEvent("deviceready");
+    },
+    receivedEvent: function(id) {
+      var listeningElement, parentElement, receivedElement;
+      parentElement = document.getElementById(id);
+      listeningElement = parentElement.querySelector(".listening");
+      receivedElement = parentElement.querySelector(".received");
+      listeningElement.setAttribute("style", "display:none;");
+      receivedElement.setAttribute("style", "display:block;");
+      return console.log("Received Event: " + id);
+    }
+  };
+
+  app.initialize();
+
+}).call(this);
